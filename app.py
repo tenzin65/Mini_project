cf39fe5601fd6dde2d64e356137fbd74b93e6007
tenzin65 <tdakar40@gmail.com>
2025-03-13 11:35:06 +0530

    Delete demo.txt file

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
